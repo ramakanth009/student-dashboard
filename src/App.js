953b979dd34399d19229fee97ebfaffc6b23ed81
ramakanth009 <ramakanthram014@gmai.com>
2024-10-18 21:04:53 +0530

    dynamic buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Sidenav from './Components/Sidenav/Sidenav';
+import Button1 from './Elements/Button1/Button1';
+import Button2 from './Elements/Button2/Button2';
+import Card1 from './Elements/Card1/Card1';
+import Card2 from './Elements/Card2/Card2';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div>
-      <Sidenav />
+      {/* <Sidenav /> */}
+      {/* <Card1/> */}
+      {/* <Card1/> */}
+      {/* <Button2/> */}
+      {/* <Home/> */}
     </div>
   );
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useStyles } from './Home.styles'
+
+const Home = () => {
+    const classes = useStyles();
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
