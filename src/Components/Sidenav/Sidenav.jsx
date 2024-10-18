b4971809eeebd9d6fa172b69ec95168c2b420e95
ramakanth009 <ramakanthram014@gmai.com>
2024-10-18 19:01:01 +0530

    update

--- src/Components/Sidenav/Sidenav.jsx
+++ src/Components/Sidenav/Sidenav.jsx
@@ -1,40 +1,198 @@
-import React from 'react';
+import React from "react";
 import { useStyles } from "./Sidenav.styles";
-import { Box, List, ListItem, ListItemText, Link } from "@mui/material";
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
-import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
-import InsertChartRoundedIcon from '@mui/icons-material/InsertChartRounded';
-import TaskAltRoundedIcon from '@mui/icons-material/TaskAltRounded';
-import LiveHelpRoundedIcon from '@mui/icons-material/LiveHelpRounded';
-
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Link,
+  Typography,
+} from "@mui/material";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
+import InsertChartRoundedIcon from "@mui/icons-material/InsertChartRounded";
+import TaskAltRoundedIcon from "@mui/icons-material/TaskAltRounded";
+import LiveHelpRoundedIcon from "@mui/icons-material/LiveHelpRounded";
+import { Avatar } from "@mui/material";
+import Dashlogo from "../../Assets/dashlogo.png";
+import LightModeRoundedIcon from "@mui/icons-material/LightModeRounded";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 
 const Sidenav = () => {
   const classes = useStyles();
 
   return (
     <Box className={classes.sidenav}>
+      <Avatar
+        sx={{ width: "250px", height: "70px" }}
+        variant="square"
+        src={Dashlogo}
+      ></Avatar>
       <List>
-        <ListItem button component={Link} to="#">
-            <HomeRoundedIcon className={classes.sidebarlogos}/>
-          <ListItemText primary="Home" />
+        <ListItem
+          button
+          component={Link}
+          to="#"
+          sx={{
+            padding: "20px 20px",
+            backgroundColor: "transparent", // Default background
+            "&:hover": {
+              backgroundColor: "#eeb015af", // Color on hover
+            },
+            "&.Mui-selected": {
+              backgroundColor: "#eeb015af", // Color when active
+            },
+          }}
+        >
+          <HomeRoundedIcon
+            className={classes.sidebarlogos}
+            sx={{ fontSize: "2rem" }}
+          />
+          <Typography
+            variant="body2"
+            color="initial"
+            className={classes.sidebuttons}
+          >
+            Home
+          </Typography>
+          <ListItemText />
         </ListItem>
-        <ListItem button component={Link} to="#">
-            <PersonRoundedIcon className={classes.sidebarlogos}/>
-          <ListItemText primary="Profile" />
+        <ListItem
+          button
+          component={Link}
+          to="#"
+          sx={{
+            padding: "20px 20px",
+            backgroundColor: "transparent", // Default background
+            "&:hover": {
+              backgroundColor: "#eeb015af", // Color on hover
+            },
+            "&.Mui-selected": {
+              backgroundColor: "#eeb015af", // Color when active
+            },
+          }}
+        >
+          <PersonRoundedIcon
+            className={classes.sidebarlogos}
+            sx={{ fontSize: "2rem" }}
+          />
+          <Typography
+            variant="body2"
+            color="initial"
+            className={classes.sidebuttons}
+          >
+            Profile
+          </Typography>
+          <ListItemText />
         </ListItem>
-        <ListItem button component={Link} to="#">
-            <InsertChartRoundedIcon className={classes.sidebarlogos}/>
-          <ListItemText primary="Curriculum" />
+        <ListItem
+          button
+          component={Link}
+          to="#"
+          sx={{
+            padding: "20px 20px",
+            backgroundColor: "transparent", // Default background
+            "&:hover": {
+              backgroundColor: "#eeb015af", // Color on hover
+            },
+            "&.Mui-selected": {
+              backgroundColor: "#eeb015af", // Color when active
+            },
+          }}
+        >
+          <InsertChartRoundedIcon
+            className={classes.sidebarlogos}
+            sx={{ fontSize: "2rem" }}
+          />
+          <Typography
+            variant="body2"
+            color="initial"
+            className={classes.sidebuttons}
+          >
+            Curriculum
+          </Typography>
+          <ListItemText />
         </ListItem>
-        <ListItem button component={Link} to="#">
-            <TaskAltRoundedIcon className={classes.sidebarlogos}/>
-          <ListItemText primary="Task" />
+        <ListItem
+          button
+          component={Link}
+          to="#"
+          sx={{
+            padding: "20px 20px",
+            backgroundColor: "transparent", // Default background
+            "&:hover": {
+              backgroundColor: "#eeb015af", // Color on hover
+            },
+            "&.Mui-selected": {
+              backgroundColor: "#eeb015af", // Color when active
+            },
+          }}
+        >
+          <TaskAltRoundedIcon
+            className={classes.sidebarlogos}
+            sx={{ fontSize: "2rem" }}
+          />
+          <Typography
+            variant="body2"
+            color="initial"
+            className={classes.sidebuttons}
+          >
+            Task
+          </Typography>
+          <ListItemText />
         </ListItem>
-        <ListItem button component={Link} to="#">
-            <LiveHelpRoundedIcon className={classes.sidebarlogos}/>
-          <ListItemText primary="Help" />
+        <ListItem
+          button
+          component={Link}
+          to="#"
+          sx={{
+            padding: "20px 20px",
+            backgroundColor: "transparent", // Default background
+            "&:hover": {
+              backgroundColor: "#eeb015af", // Color on hover
+            },
+            "&.Mui-selected": {
+              backgroundColor: "#eeb015af", // Color when active
+            },
+          }}
+        >
+          <LiveHelpRoundedIcon
+            className={classes.sidebarlogos}
+            sx={{ fontSize: "2rem" }}
+          />
+          <Typography
+            variant="body2"
+            color="initial"
+            className={classes.sidebuttons}
+          >
+            Help
+          </Typography>
+          <ListItemText />
         </ListItem>
       </List>
+      <Box className={classes.sidebottom}>
+        <LightModeRoundedIcon className={classes.bottomlogos} />
+        <Typography
+          variant="body2"
+          color="initial"
+          className={classes.logout}
+          sx={{
+            backgroundColor: "transparent", // Default background
+            "&:hover": {
+              padding: "8px 8px",
+              borderRadius:"20px",
+              backgroundColor: "#eeb015af", // Color on hover
+
+            },
+            "&.Mui-selected": {
+              backgroundColor: "#eeb015af", // Color when active
+            },
+          }}
+        >
+          <LogoutRoundedIcon />
+          Logout
+        </Typography>
+      </Box>
     </Box>
   );
 };
--- src/Components/Sidenav/Sidenav.styles.jsx
+++ src/Components/Sidenav/Sidenav.styles.jsx
@@ -1,12 +1,44 @@
 import { makeStyles } from "@mui/styles";
 
 export const useStyles = makeStyles(() => ({
-    sidenav: {
-        width: '250px',
-        height: '100vh',
-        backgroundColor: '#f0f0f0',
-      },
-      sidebarlogos:{
-        paddingRight:"10px"
-      }
-}))
+  sidenav: {
+    width: "250px",
+    height: "100vh",
+    backgroundColor: "#f0f0f0",
+  },
+  sidelist: {
+    padding: "200px 20px",
+  },
+  sidebarlogos: {
+    paddingRight: "10px",
+    color: "#555353",
+    fontSize: "2rem",
+  },
+  sidebuttons: {
+    "&.MuiTypography-root": {
+      fontSize: "1.3rem", // Ensure this applies specifically to Typography
+      color: "#555353",
+    },
+  },
+  sidebottom: {
+    display: "flex",
+    alignItems: "center",
+    gap:"50px",
+    position:"absolute",
+    bottom:"10px",
+    padding:"10px 10px 10px 20px"
+  },
+  logout: {
+    display:"flex",
+    alignItems:"center",
+    "&.MuiTypography-root": {
+      fontSize: "1.6rem", // Ensure this applies specifically to Typography
+      color: "#555353",
+    },
+  },
+  bottomlogos: {
+    paddingRight: "10px",
+    color: "#555353",
+    fontSize: "2rem",
+  },
+}));
