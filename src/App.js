59158b5e0f71c861e37e584372a3154970ad85a2
tulasi <stndatthu1994@gmail.com>
2024-10-19 11:46:18 +0530

    fixed route issues

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
 import Sidenav from "./components/Sidenav/sidenav";
 import AppRoutes from "./routes/routes"; // Import the routes
 
 function App() {
   return (
-    <Router>
-      <div style={{ display: "flex" }}>
-        {/* Sidenav is always visible */}
-        <Sidenav />
-
-        {/* Main content area */}
-        <div style={{ flexGrow: 1, padding: "20px" }}>
-          <AppRoutes /> {/* Render the imported routes */}
-        </div>
+    <div style={{ display: "flex" }}>
+      {/* Sidenav is always visible */}
+      <Sidenav />
+      <div style={{ flexGrow: 1, padding: "20px" }}>
+        <AppRoutes /> {/* Render the imported routes */}
       </div>
-    </Router>
+    </div>
   );
 }
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/home"; // Import the Home page component
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route>
-        <Route path="/" element={<Home />} /> {/* Home Page */}
-      </Route>
+      <Route path="/" element={<Home />} /> {/* Home Page */}
     </Routes>
   );
 };
